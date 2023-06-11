3f0748c4cec53391c597ec39e49f260952abe972
Jake Martin <7m.jake@gmail.com>
2023-06-11 14:43:50 -0400

    Consolidated 'complete' logic into conditions of a single toggle

--- Updates/TaskListView.swift
+++ Updates/TaskListView.swift
@@ -15,43 +15,6 @@ struct TaskRow: View {
     @State private var currentUpdate: Update?
     @State var taskChecked = Bool()
     
-    
-    
-//    func fetchUpdate(for task: Task) -> Update {
-//        let fetchRequest: NSFetchRequest<Update> = Update.fetchRequest()
-//        
-//        // Get the start of the day for the date argument
-//        let startOfDay = Calendar.current.startOfDay(for: selectedDate.date)
-//        
-//        // This predicate assumes 'date' is a Date type and 'task' is a relationship to the Task entity
-//        let predicate = NSPredicate(format: "date == %@ AND task == %@", startOfDay as NSDate, task)
-//        
-//        fetchRequest.predicate = predicate
-//        
-//        do {
-//            let updates = try context.fetch(fetchRequest)
-//            if let update = updates.first {
-//                // Return the first update if one exists
-//                return update
-//            }
-//        } catch {
-//            print("Failed to fetch Update: \(error)")
-//        }
-//        
-//        // If no Update was found or an error occurred, create a new Update
-//        let newUpdate = Update(context: context)
-//        newUpdate.date = startOfDay
-//        newUpdate.task = task
-//        
-//        do {
-//            try context.save()
-//        } catch {
-//            print("Failed to save new Update: \(error)")
-//        }
-//        
-//        return newUpdate
-//    }
-    
     func fetchExistingUpdate(for task: Task) -> Update? {
         let fetchRequest: NSFetchRequest<Update> = Update.fetchRequest()
         
@@ -82,13 +45,13 @@ struct TaskRow: View {
         let newUpdate = Update(context: context)
         newUpdate.date = Calendar.current.startOfDay(for: selectedDate.date)
         newUpdate.task = task
-
+        
         do {
             try context.save()
         } catch {
             print("Failed to save new Update: \(error)")
         }
-
+        
         return newUpdate
     }
     
@@ -109,16 +72,28 @@ struct TaskRow: View {
             
             HStack {
                 
-                //Completed Task
-                if task.complete {
-                    Toggle(isOn: $taskChecked, label: {
-                        Text(task.name ?? "error")
-                            .strikethrough()
-                    })
-                    .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                    
-                    
-                    Spacer()
+                //Task checkbox
+                Toggle(isOn: $taskChecked, label: {
+                    Text(task.name ?? "error")
+                })
+                .disabled(task.complete)
+                .onChange(of: taskChecked) { newValue in
+                    let update = fetchExistingUpdate(for: task)
+                    if newValue == false && fetchExistingUpdate(for: task) != nil{
+                        context.delete(update!)
+                        print("update deleted")
+                    }
+                }
+                
+                Spacer()
+                
+                //'Complete button' logic
+                if taskChecked && !task.complete {
+                    Button("Mark as Complete") {
+                        task.complete = true
+                    }
+                    .buttonStyle(.link)
+                } else if taskChecked && task.complete {
                     Button("Undo") {
                         task.complete = false
                     }
@@ -134,42 +109,8 @@ struct TaskRow: View {
                     }
                     .buttonStyle(.link)
                     .foregroundColor(.red)
-                    
-                    
                 }
                 
-                
-                //Uncompleted Task
-                else if !task.complete {
-                    Toggle(isOn: $taskChecked, label: {
-                        Text(task.name ?? "error")
-                    })
-                    .disabled(task.complete)
-                    
-                    .onChange(of: taskChecked) { newValue in
-                        let update = fetchExistingUpdate(for: task)
-                        if newValue == false && fetchExistingUpdate(for: task) != nil{
-                            context.delete(update!)
-                            print("update deleted")
-                        }
-                    }
-                    
-                    // Update debug test
-//                    if fetchExistingUpdate(for: task) != nil {
-//                        //let taskChecked = true
-//                        Text("update here!")
-//                            .foregroundColor(Color.green)
-//                    }
-                    
-                    Spacer()
-                    
-                    if taskChecked {
-                        Button("Mark as Complete") {
-                            task.complete = true
-                        }
-                        .buttonStyle(.link)
-                    }
-                }
                 
                 
             }
@@ -184,7 +125,7 @@ struct TaskRow: View {
                         
                     }
                     .onChange(of: updateContent) { newValue in
-        
+                        
                         
                         // Update the Update's content whenever updateContent changes
                         currentUpdate?.content = newValue
@@ -209,7 +150,7 @@ struct TaskRow: View {
                     }
                     .lineLimit(2...)
                     .disabled(task.complete)
-                    
+                
                 
             }
         }
@@ -220,7 +161,7 @@ struct TaskRow: View {
             setTaskChecked()
         }
     }
-        
+    
 }
 
 
@@ -321,15 +262,15 @@ struct TaskListView: View {
                 }
             }
             
-//            Button(action: {
-//                deleteAllUpdates()
-//            }, label: {
-//                Text("Delete All Updates")
-//            })
-//            .background(
-//                RoundedRectangle(cornerRadius: 8, style: .continuous)
-//                    .fill(Color.red)
-//            )
+            //            Button(action: {
+            //                deleteAllUpdates()
+            //            }, label: {
+            //                Text("Delete All Updates")
+            //            })
+            //            .background(
+            //                RoundedRectangle(cornerRadius: 8, style: .continuous)
+            //                    .fill(Color.red)
+            //            )
         }
     }
     
